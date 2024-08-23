b4d024342d58d60a7603988158266b55393048b6
gustavo <gustavogarciaci1@gmail.com>
2024-08-23 17:18:29 -0300

    feat: zod validations

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,16 +4,28 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  passwordHash: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, passwordHash } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
@@ -26,11 +38,13 @@ export class CreateAccountController {
       throw new BadRequestException('User with same email already exists')
     }
 
+    const hashedPassword = await hash(passwordHash, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        passwordHash,
+        passwordHash: hashedPassword,
       },
     })
   }
