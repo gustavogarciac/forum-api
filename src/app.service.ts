a3e27f9fb7cdcaeebb35279f613e3145fa2506c9
gustavo <gustavogarciaci1@gmail.com>
2024-08-23 16:57:48 -0300

    feat: create account controller

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common'
-
-@Injectable() // This decorator means that other files may create dependency on this class
-export class AppService {
-  getHello(): string {
-    return 'Hello World!'
-  }
-}
