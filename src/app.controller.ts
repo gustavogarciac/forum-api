5a523b64bfd6f3bed5a86b36f5c18fd020b774fd
gustavo <gustavogarciaci1@gmail.com>
2024-08-23 16:01:45 -0300

    chore: compose run

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
 
-@Controller()
+import { AppService } from './app.service'
+
+@Controller() // Controller decorator parameters will be route prefixes for every route in the controller.
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // HTTP method decorator parameters will be route suffixes for the route.
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
-@Injectable()
+@Injectable() // This decorator means that other files may create dependency on this class
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
