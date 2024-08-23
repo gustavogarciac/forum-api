29a52e48ea91edc7bd8c82f9942fce9a8697d81f
gustavo <gustavogarciaci1@gmail.com>
2024-08-23 20:39:59 -0300

    feat: e2e testiing setup

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -4,13 +4,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
   },
-  plugins: [
-    swc.vite({
-      module: { type: 'es6' },
-    }),
-    tsconfigPaths.default(),
-  ],
+  plugins: [swc.vite(), tsconfigPaths.default()],
 })
