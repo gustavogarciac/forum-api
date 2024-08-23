b7763c60c8a32b9d8802f4eaf5a819000e15d14f
gustavo <gustavogarciaci1@gmail.com>
2024-08-23 18:24:21 -0300

    feat: jwt rs256 algorithm

--- src/env.ts
+++ src/env.ts
@@ -5,8 +5,10 @@ export const envSchema = z.object({
   NEST_CLEAN_DATABASE_USER: z.string(),
   NEST_CLEAN_DATABASE_PASSWORD: z.string(),
   NEST_CLEAN_DATABASE_NAME: z.string(),
-  DATABASE_URL: z.string(),
+  DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
