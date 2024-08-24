192856e66533756d9bf92d965cad2daaa8ec0173
gustavo <gustavogarciaci1@gmail.com>
2024-08-24 19:19:26 -0300

    feat: encryption and hashing stubs

---
+++ test/cryptography/fake-hasher.ts
@@ -0,0 +1,14 @@
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+export class FakeHasher implements HashGenerator, HashComparer {
+  async hash(plain: string): Promise<string> {
+    return plain.concat('hashed')
+  }
+
+  async compare(plain: string, hashed: string): Promise<boolean> {
+    const equals = plain.concat('hashed') === hashed
+
+    return equals
+  }
+}
