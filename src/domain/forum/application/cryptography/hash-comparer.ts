95a18ae15c67069310f474ea0157a2c969127172
gustavo <gustavogarciaci1@gmail.com>
2024-08-24 19:15:51 -0300

    feat: authenticate student and register student use cases

--- src/domain/forum/application/cryptography/hash-comparer.ts
+++ src/domain/forum/application/cryptography/hash-comparer.ts
@@ -1,3 +1,18 @@
-export abstract class HashGenerator {
+export abstract class HashComparer {
+  /**
+   * @abstract compare
+   * @param {string} plain
+   * @param {string} hashed
+   * @return {Promise<boolean>}
+   * @description Method to compare a plain text with a hashed text
+   * @async
+   * @access public
+   * @example
+   * const plain = 'plain_text'
+   * const hashed = 'hashed_text'
+   * const response = await hashComparer.compare(plain, hashed)
+   * console.log(response) // Output: false
+   * @throws {Error} This method must be implemented
+   * **/
   abstract compare(plain: string, hashed: string): Promise<boolean>
 }
