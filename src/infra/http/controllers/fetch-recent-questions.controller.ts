04ca008a70232fa0ab0f2bb75fb589ac8150548f
gustavo <gustavogarciaci1@gmail.com>
2024-08-24 21:02:13 -0300

    feat: global jwt verification

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,15 +1,8 @@
 /* eslint-disable no-useless-escape */
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Query,
-  UseGuards,
-} from '@nestjs/common'
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common'
 import { z } from 'zod'
 
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
-import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 
 import { QuestionPresenter } from '../presenters/question-presenter'
@@ -37,7 +30,6 @@ const paginationQueryValidationPipe = new ZodValidationPipe(
 )
 
 @Controller('/questions')
-@UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
   constructor(
     private fetchRecentQuestionsUseCase: FetchRecentQuestionsUseCase,
