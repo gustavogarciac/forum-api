d3e2c3e348359e9d06ce54809b861a8be4de72c2
gustavo <gustavogarciaci1@gmail.com>
2024-08-25 17:39:18 -0300

    feat: delete answer controller

--- src/domain/forum/application/use-cases/delete-answer.ts
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,3 +1,5 @@
+import { Injectable } from '@nestjs/common'
+
 import { Either, left, right } from '@/core/either'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
@@ -14,6 +16,7 @@ type DeleteAnswerUseCaseResponse = Either<
   object
 >
 
+@Injectable()
 export class DeleteAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
